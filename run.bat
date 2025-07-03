f285f457779989a1b42ad207227e7a9a5e351105
Bram <145452823+BramHanze@users.noreply.github.com>
2025-07-03 22:58:06 +0200

    automatically create the .venv when it's missing

--- run.bat
+++ run.bat
@@ -2,6 +2,14 @@
 
 cd /d %~dp0\
 
+IF NOT EXIST ".venv" (
+    python -m venv .venv
+
+    call .venv\Scripts\activate.bat
+
+    pip install -r .\requirements.txt
+)
+
 call .venv/Scripts/Activate.bat
 
 start "" http://127.0.0.1:8000
